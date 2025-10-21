10908dd9e54e27eeeb9edb47841fec1f586abb44
Karam Abbas <kma69362@gmail.com>
2025-10-21 15:25:46 -0700

    commit

---
+++ routes/students.js
@@ -0,0 +1,12 @@
+import {Router} from 'express'
+
+const router = Router()
+
+
+
+// Homepage: GET /students?search=term$cohort=Year%201
+router.get('/', async (req, res) =>{
+    
+})
+
+export default Router
